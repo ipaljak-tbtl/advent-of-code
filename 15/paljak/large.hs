ed016704f0c8d31e4675c0543897f63e7d7980be
Ivan Paljak <ivanp@tbtl.com>
2020-12-15 10:59:22 +0100

    Add day 15 solutions

---
+++ 15/paljak/large.hs
@@ -0,0 +1,24 @@
+import Data.List
+import Data.List.Split
+
+import qualified Data.Map as Map
+
+data State = State (Map.Map Int Int) Int deriving Show
+
+getState :: [Int] -> State
+getState xs = State (history xs) (last xs)
+  where
+    history xs      = foldl' (insert) Map.empty $ zip (init xs) [1..]
+    insert m (k, v) = Map.insert k v m
+
+findNum :: Int -> State -> Int
+findNum 30000000 (State _ last) = last
+findNum n    (State m last)
+  | last `Map.notMember` m = findNum (n + 1) (State m' 0)
+  | otherwise              = findNum (n + 1) (State m' (n - m Map.! last))
+    where m' = Map.insert last n m
+
+main :: IO ()
+main =
+  getContents >>= print . findNum 6 . getState .
+                  map (read::String->Int) . splitOn "," . head . lines
