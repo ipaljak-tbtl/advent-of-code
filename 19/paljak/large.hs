d6c10946ef4fad1c54656302fe20a5d781139824
Ivan Paljak <ivanp@tbtl.com>
2020-12-19 08:37:56 +0100

    Add day 19 solutions

---
+++ 19/paljak/large.hs
@@ -0,0 +1,53 @@
+import Data.List
+import Data.List.Split
+
+import qualified Data.Map as Map
+
+import Debug.Trace
+
+type RuleId = Int
+data Rule   = Leaf String | Concat [RuleId] | Any [Rule] deriving Show
+
+parseRule :: String -> (Int, Rule)
+parseRule s = (ruleId, rule)
+  where
+    ruleId = read . head $ splitOn ":" s
+    rhs    = tail . last $ splitOn ":" s
+    rule
+     | '"' `elem` rhs = Leaf $ read rhs
+     | '|' `elem` rhs = Any . map (Concat . parseConcat) $ splitOn "|" rhs
+     | otherwise      = Concat $ parseConcat rhs
+     where parseConcat = map (read) . words
+
+parseRules :: [String] -> Map.Map Int Rule
+parseRules = foldl' (\m (k, v) -> Map.insert k v m) Map.empty . map (parseRule)
+
+parseLines :: [String] -> (Map.Map Int Rule, [String])
+parseLines l = (parseRules ruleLines, wordLines)
+  where
+    ruleLines = take 134 l
+    wordLines = drop 135 l
+
+maxLen :: Map.Map Int Rule -> Rule -> Int
+maxLen m (Any [Concat [42], Concat [42, 8]]) = 100
+maxLen m (Any [Concat [42, 31], Concat [42, 11, 31]]) = 100
+maxLen m (Leaf _)     = 1
+maxLen m (Concat ids) = sum $ map (\i -> maxLen m (m Map.! i)) ids
+maxLen m (Any rs)     = maximum $ map (maxLen m) rs
+
+match :: Map.Map Int Rule -> String -> Rule -> Bool
+match m t (Leaf s)     = s == t
+match m t r@(Any rs)   = length t <= maxLen m r && any (match m t) rs
+match m "" (Concat []) = True
+match m "" (Concat _)  = False
+match m t (Concat [])  = False
+match m t (Concat (i:[])) = match m t $ m Map.! i
+match m t r@(Concat ids)  = length t <= maxLen m r && any (matchPref) [0..length t]
+  where matchPref i = (match m (take (i + 1) t) $ m Map.! (head ids)) &&
+                      (match m (drop (i + 1) t) (Concat $ tail ids))
+
+matching :: (Map.Map Int Rule, [String]) -> [String]
+matching (rules, ts) = filter (\t -> trace t $ match rules t (rules Map.! 0)) ts
+
+main :: IO ()
+main = getContents >>= print . length . matching . parseLines . lines
