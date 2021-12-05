b12536d73b09e281d13a074fa3aafd89b64004c1
Ivan Paljak <ivanp@tbtl.com>
2021-12-05 09:38:07 +0100

    Source limit exceeded

--- 2021/paljak/5/large.cpp
+++ 2021/paljak/5/large.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 const int MAX = 1000;
 
+int a, b, c, d, sol;
 int cnt[MAX][MAX];
 
 int vec(int a, int b) {
@@ -12,19 +13,13 @@ int vec(int a, int b) {
 }
 
 int main(void) {
-  int a, b, c, d;
   while (scanf("%d,%d -> %d,%d", &a, &b, &c, &d) != EOF) {
     int dx = vec(a, c), dy = vec(b, d);
     while (a + dx != c || b + dy != d) {
-      ++cnt[c][d];
+      sol += (++cnt[c][d]) == 2;
       c += dx, d += dy;
     };
   }
-
-  int sol = 0;
-  for (int i = 0; i < MAX; ++i)
-    for (int j = 0; j < MAX; ++j)
-      sol += cnt[i][j] > 1;
 
   printf("%d\n", sol);
 
