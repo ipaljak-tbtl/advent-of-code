12ed80f8f4b65156d35d79a450f554ca84b6fb7c
Ante Derek <ante.derek@gmail.com>
2021-12-05 17:05:41 +0100

    Day 5 fix

--- 2021/ante/d5/d5a.cpp
+++ 2021/ante/d5/d5a.cpp
@@ -12,8 +12,8 @@ int main() {
   while (scanf("%d,%d -> %d,%d", &x1, &y1, &x2, &y2) == 4) {
     if (x1 != x2 && y1 != y2)
       continue ;
-    for (int x=min(x1, x2); x<=max(x1, x2); x1!=x2?x++)
-      for (int y=min(y1, y2); y<=max(y1, y2); y1!=y2?y++)
+    for (int x=min(x1, x2); x<=max(x1, x1); x++)
+      for (int y=min(y1, y2); y<=max(y1, y2); y++)
         freq[{x, y}]++;
   }
   
