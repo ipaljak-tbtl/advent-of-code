4062180e6c1dedabb66588bc7a11d41f7e20c4dc
Ante Derek <ante.derek@gmail.com>
2021-12-06 09:00:15 +0100

    Day 6 solutions

---
+++ 2021/ante/d6/d6b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define TRACE(x) cerr << #x << " = " << x << endl
+#define _ << " _ " << 
+
+using namespace std;
+
+vector<int> read_csv() {
+  string line;
+  vector<int> result;
+  if (!(cin >> line))
+    return result;
+  istringstream in(line);
+  int value = 0;
+  while (in >> value) {
+    result.push_back(value);
+    char c;
+    in >> c;
+    assert(c == ',');
+  }
+  return result;
+}
+
+int main() {
+  vector<int> numbers = read_csv();
+  vector<unsigned long long> state(9, 0);
+  for (auto &x : numbers)
+    state[x]++;
+  for (int i=0, s=0; i<256; i++, s=(s+1)%9)
+    state[(s+7)%9] += state[s];
+  unsigned long long sum = 0;
+    for (auto &x : state)
+    sum += x;
+  cout << sum << endl;
+  return 0;
+}
