85b61cf62a8bf77d744989fe6fdeffe47fb7dabe
Matija Bertovic <matija@tbtl.com>
2022-12-06 09:39:56 +0100

    Add 2022 day 06 solutions

---
+++ 2022/matija/06/src/part2.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools as _;
+
+pub fn solve(input: &str) -> usize {
+    let n = 14;
+    let mut queue = std::collections::VecDeque::new();
+
+    for c in input.chars().take(n) {
+        queue.push_front(c);
+    }
+
+    if queue.iter().unique().count() == n {
+        return n;
+    }
+
+    for (i, c) in input.char_indices().skip(n) {
+        queue.pop_back();
+        queue.push_front(c);
+
+        if queue.iter().unique().count() == n {
+            return i + 1;
+        }
+    }
+
+    0
+}
