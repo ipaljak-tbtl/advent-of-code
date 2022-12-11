34b8ca12efae209dd3fca4428cb20410fbbbff7e
Dino Santl <dino@tbtl.com>
2022-12-11 19:45:45 +0100

    Add solution for day6

---
+++ 2022/dsantl/day_6/src/part1.rs
@@ -0,0 +1,26 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let mut memo: Vec<usize> = vec![0; 26];
+        let mut buff: Vec<char> = vec![];
+        let line = line.unwrap();
+        for (index, c) in line.chars().enumerate() {
+            if buff.len() == 4 {
+                let reset = buff.remove(0);
+                let ord = (reset as usize) - ('a' as usize);
+                memo[ord] -= 1;
+            }
+            let ord = (c as usize) - ('a' as usize);
+            memo[ord] += 1;
+            buff.push(c);
+            let sum = memo.iter().filter(|x| **x == 1usize).sum::<usize>();
+            if sum == 4 {
+                println!("{}", index + 1);
+                break;
+            }
+        }
+    }
+}
