bfdd5a0639419ab195a0784fc3caf33d0446109f
Miroslav Filipovic <miroslav@tbtl.com>
2023-12-09 09:16:12 +0100

    [AOC2023] DAY-09 mirof solution

---
+++ 2023/mirof/day09/first.hs
@@ -0,0 +1,28 @@
+import Text.Read
+
+---------------- Parsing ---------------
+
+parse :: String -> [[Integer]]
+parse = map (map (\num -> read num :: Integer) . words) . lines
+
+----------------- Solve ----------------
+
+solve :: [[Integer]] -> Integer
+solve = sum . map interpolate_new_value 
+
+interpolate_new_value :: [Integer] -> Integer
+interpolate_new_value numbers
+    | all (== 0) numbers = 0
+    | otherwise          = (last numbers) + (interpolate_new_value . diffs) numbers
+
+diffs :: [Integer] -> [Integer]
+diffs [last]                     = []
+diffs (num1:rest@(num2:numbers)) = (num2 - num1) : diffs rest
+        
+------------------ Main ----------------
+
+main = do
+    input <- readFile "input.txt"
+    let solution = solve $ parse input
+    putStrLn $ show solution
+
