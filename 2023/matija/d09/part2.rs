6326f490afdc8d5c823404564f680bd2096d9e43
Matija Bertovic <matija@tbtl.com>
2023-12-09 12:03:11 +0100

    Add 2023 day 9 solutions

---
+++ 2023/matija/d09/part2.rs
@@ -0,0 +1,41 @@
+fn solve(reader: impl std::io::BufRead) -> i64 {
+    let mut result = 0i64;
+
+    for line in reader.lines().map(Result::unwrap) {
+        let mut seq: Vec<i64> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .rev()
+            .collect();
+
+        let mut len = seq.len();
+
+        while seq.iter().take(len).any(|&x| x != 0) {
+            for i in 0..(len - 1) {
+                seq[i] = seq[i + 1] - seq[i];
+            }
+            len -= 1;
+        }
+
+        result += seq.iter().sum::<i64>();
+    }
+
+    result
+}
+
+fn main() {
+    println!("{}", solve(std::io::stdin().lock()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use std::{fs::File, io::BufReader};
+
+    #[test]
+    fn test_d09p2() {
+        let reader = BufReader::new(File::open("d09/test_input.txt").unwrap());
+        let result = solve(reader);
+        assert_eq!(result, 2);
+    }
+}
