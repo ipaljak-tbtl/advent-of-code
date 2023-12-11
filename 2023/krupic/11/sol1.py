4c1d51eac1d1debb5f88d350858cb76fc2220249
Kristijan Rupić <kristijan@tbtl.com>
2023-12-11 13:08:32 +0100

    [AoC2023] krupic - day 11

---
+++ 2023/krupic/11/sol1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+grid = list(map(str.strip, sys.stdin))
+rows = len(grid)
+cols = len(grid[0])
+galaxies = [(i,j) for i in range(rows) for j in range(cols) if grid[i][j] == '#']
+
+empty_rows = [i for i in range(rows) if all(map(lambda x: x == '.', grid[i]))]
+empty_cols = [j for j in range(cols) if all(grid[i][j] == '.' for i in range(rows))]
+
+#print(galaxies)
+#print(empty_rows)
+#print(empty_cols)
+xs, ys = map(sorted, zip(*galaxies))
+
+sol = 0
+
+for i, x_larger in enumerate(xs):
+    for x_smaller in xs[:i]:
+        expansion = sum(1 for x in empty_rows if x_smaller < x < x_larger)
+        sol += x_larger - x_smaller + expansion
+
+
+for i, y_larger in enumerate(ys):
+    for y_smaller in ys[:i]:
+        expansion = sum(1 for y in empty_cols if y_smaller < y < y_larger)
+        sol += y_larger - y_smaller + expansion
+
+print(sol)
+
