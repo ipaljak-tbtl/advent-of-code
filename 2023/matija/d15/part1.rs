287a6289dd7a3bfc25873ebb026b50f261f6562b
Matija Bertovic <matija@tbtl.com>
2023-12-15 11:48:41 +0100

    Add 2023 day 15 part1 test

--- 2023/matija/d15/part1.rs
+++ 2023/matija/d15/part1.rs
@@ -12,3 +12,16 @@ fn solve(reader: impl std::io::BufRead) -> u32 {
 fn main() {
     println!("{}", solve(std::io::stdin().lock()));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use std::{fs::File, io::BufReader};
+
+    #[test]
+    fn test_d15p1() {
+        let reader = BufReader::new(File::open("d15/test_input.txt").unwrap());
+        let result = solve(reader);
+        assert_eq!(result, 1320);
+    }
+}
