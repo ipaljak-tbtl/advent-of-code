f2362a872f7833e30eac234e1ebb6bd1c1fb6120
Ivan Paljak <ivanp@tbtl.com>
2023-12-16 09:46:32 +0100

    Add Day 16 2023 solutions

---
+++ 2023/paljak/16/small.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAXN = 150;
+
+// RDLU
+const int dr[] = {0, 1, 0, -1};
+const int ds[] = {1, 0, -1, 0};
+
+int n;
+
+bool been[MAXN][MAXN][MAXN];
+char grid[MAXN][MAXN];
+
+inline bool check(const int r, const int s) {
+  return min(r, s) >= 0 && max(r, s) < n;
+}
+
+void dfs(int r, int s, int dir) {
+  if (!check(r, s)) return;
+  if (been[r][s][dir]) return;
+  been[r][s][dir] = true;
+
+  if (grid[r][s] == '.' ||
+     (grid[r][s] == '|' && dir % 2 == 1) ||
+     (grid[r][s] == '-' && dir % 2 == 0)) {
+    dfs(r + dr[dir], s + ds[dir], dir);
+    return;
+  }
+
+  if (grid[r][s] == '\\') {
+    int _dir = dir ^ 1;
+    dfs(r + dr[_dir], s + ds[_dir], _dir);
+  }
+
+  if (grid[r][s] == '/') {
+    int _dir = ((((dir + 1) % 4) ^ 1) + 3) % 4;
+    dfs(r + dr[_dir], s + ds[_dir], _dir);
+  }
+
+  if (grid[r][s] == '|' || grid[r][s] == '-')
+    for (int i = 0; i < 4; ++i)
+      if (i % 2 != dir % 2)
+        dfs(r + dr[i], s + ds[i], i);
+}
+
+int main(void) {
+  while (scanf("%s", grid[n++]) == 1);
+  --n;
+
+  dfs(0, 0, 0);
+
+  int sol = 0;
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      sol += been[i][j][0] | been[i][j][1] | been[i][j][2] | been[i][j][3];
+
+  printf("%d\n", sol);
+
+  return 0;
+}
