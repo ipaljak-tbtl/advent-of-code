790d9ee2ed76e2fad7eed3d0fd067a8075c49776
Ivan Paljak <ivanp@tbtl.com>
2024-12-01 16:41:39 +0100

    Add 2024 day 1 solutions

---
+++ 2024/paljak/1/large.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+template <class T>
+using ordered_set =
+    __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag,
+                     __gnu_pbds::tree_order_statistics_node_update>;
+
+#define TRACE(x) cerr << __LINE__ << ": " << #x << " = " << x << endl
+#define _ << " _ " <<
+
+template <class T, class U> ostream &operator<<(ostream &o, pair<T, U> x) {
+  return o << "(" << x.first << ", " << x.second << ")";
+}
+template <class> struct is_container : false_type {};
+template <class... Ts> struct is_container<vector<Ts...>> : true_type {};
+template <class... Ts> struct is_container<map<Ts...>> : true_type {};
+template <class... Ts> struct is_container<unordered_map<Ts...>> : true_type {};
+template <class... Ts> struct is_container<set<Ts...>> : true_type {};
+template <class... Ts> struct is_container<unordered_set<Ts...>> : true_type {};
+template <class... Ts> struct is_container<multiset<Ts...>> : true_type {};
+template <class... Ts>
+struct is_container<unordered_multiset<Ts...>> : true_type {};
+template <class T, class = typename enable_if<is_container<T>::value>::type>
+ostream &operator<<(ostream &o, T x) {
+  int f = 1;
+  o << "{";
+  for (auto y : x) {
+    o << (f ? "" : ", ") << y;
+    f = 0;
+  }
+  return o << "}";
+}
+
+typedef long long llint;
+
+vector<int> va, vb;
+
+int main(void) {
+  int a, b;
+  while (scanf("%d%d", &a, &b) == 2) {
+    va.emplace_back(a);
+    vb.emplace_back(b);
+  }
+
+  sort(vb.begin(), vb.end());
+
+  int sol = 0;
+  for (const auto &val : va)
+    sol += val * (int)(upper_bound(vb.begin(), vb.end(), val) -
+                       lower_bound(vb.begin(), vb.end(), val));
+
+  printf("%d\n", sol);
+
+  return 0;
+}
