5f0c9be88ce940cdece40c43f0db79d06857f2c4
Kristijan Rupić <kristijan@tbtl.com>
2024-12-05 10:25:02 +0100

    [AOC2024] Kristijan day 5

---
+++ 2024/krupic/05/sol1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
+
+rules = []
+prev = defaultdict(set)
+post = defaultdict(set)
+
+while line := input().strip():
+    x, y = map(int, line.split('|'))
+    rules.append((x, y))
+    prev[y].add(x)
+    post[x].add(y)
+
+updates = [list(map(int, line.split(','))) for line in sys.stdin]
+
+def valid(update):
+    seen_prev = set()
+    for x in update:
+        if not seen_prev.isdisjoint(post[x]):
+            return False
+        seen_prev.add(x)
+
+    seen_post = set()
+    for x in reversed(update):
+        if not seen_post.isdisjoint(prev[x]):
+            return False
+        seen_post.add(x)
+
+    return True
+
+sol = 0
+for update in updates:
+    if valid(update):
+        assert len(update) % 2 == 1
+        sol += update[len(update)//2]  
+
+print(sol)
+
---
+++ 2024/krupic/05/sol2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
+
+rules = []
+prev = defaultdict(set)
+post = defaultdict(set)
+
+while line := input().strip():
+    x, y = map(int, line.split('|'))
+    rules.append((x, y))
+    prev[y].add(x)
+    post[x].add(y)
+
+updates = [list(map(int, line.split(','))) for line in sys.stdin]
+
+def valid(update):
+    seen_prev = set()
+    for x in update:
+        if not seen_prev.isdisjoint(post[x]):
+            return False
+        seen_prev.add(x)
+
+    seen_post = set()
+    for x in reversed(update):
+        if not seen_post.isdisjoint(prev[x]):
+            return False
+        seen_post.add(x)
+
+    return True
+
+def toposort(update):
+    update = set(update)
+
+    s = []
+    v = set()
+    def rec(x):
+        if x in v:
+            return
+        v.add(x)
+        for y in prev[x]:
+            if y in update:
+                rec(y)
+        s.append(x)
+
+    for x in update:
+        rec(x)
+
+    return s
+
+sol = 0
+for update in updates:
+    if not valid(update):
+        assert len(update) % 2 == 1
+        update = toposort(update)
+        sol += update[len(update)//2]  
+
+print(sol)
+
