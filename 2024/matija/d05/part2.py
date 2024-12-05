9297dc2b07f3e01300623ffaef41913fb9227189
Matija Bertovic <matija@tbtl.com>
2024-12-05 08:05:14 +0100

    [AOC2024] Matija Day 05

---
+++ 2024/matija/d05/part2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    rules = {}
+
+    line = next(sys.stdin).strip()
+    while line:
+        x, y = map(int, line.split("|"))
+
+        rules.setdefault(y, set()).add(x)
+
+        line = next(sys.stdin).strip()
+
+    total = 0
+
+    for line in sys.stdin:
+        pages = list(map(int, line.split(",")))
+
+        found = False
+
+        for i in range(1, len(pages)):
+            for j in range(i):
+                if pages[i] in rules.get(pages[j], set()):
+                    found = True
+                    while i > j:
+                        pages[i-1], pages[i] = pages[i], pages[i-1]
+                        i -= 1
+                    break
+
+        if found:
+            total += pages[len(pages)//2]
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
