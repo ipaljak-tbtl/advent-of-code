cc6ac303eebccb4329654be3ef0baa0113c4351a
Ante Derek <ante.derek@gmail.com>
2024-12-07 07:16:26 +0100

    [AOC2024] ante day07

---
+++ 2024/ante/day07/d7a.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define TRACE(x) cerr << #x << " = " << x << endl
+#define _ << " _ " << 
+
+using namespace std;
+
+
+typedef long long llint;
+
+
+bool possible(vector<llint> &s, llint target) {
+  queue<pair<int, llint>> q;
+  q.push({1, s[0]});
+  while (!q.empty()) {
+    auto p = q.front(); q.pop();
+
+    int i = p.first;
+    llint y = p.second;
+  
+    if (y == target && i == (int)s.size())
+      return true;
+
+    if (i >= (int)s.size())
+      continue; 
+
+    q.push({i+1, y + s[i]});
+    q.push({i+1, y * s[i]});
+  }
+  return false;
+}
+
+
+int main() {
+  llint total = 0;
+  
+  string line;
+  while (getline(cin, line)) {
+    istringstream in(line);
+    long long target;
+    char c;
+    in >> target >> c;
+    assert(c == ':');
+    vector<llint> s;
+    int x;
+    while (in >> x)
+      s.push_back(x);
+    if (possible(s, target))
+      total += target;
+  }
+
+  cout << total << endl;
+  return 0;
+}
