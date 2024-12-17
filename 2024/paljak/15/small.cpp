ea9e9d793c12e0baf95e4418ab960139b28286a9
Ivan Paljak <ivanp@tbtl.com>
2024-12-18 00:01:22 +0100

    Add 2024 day 15 solutions

---
+++ 2024/paljak/15/small.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+template <class T>
+using ordered_set =
+    __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag,
+                     __gnu_pbds::tree_order_statistics_node_update>;
+
+#define TRACE(x) cerr << __LINE__ << ": " << #x << " = " << x << endl
+#define _ << " _ " <<
+
+template <class T, class U> ostream &operator<<(ostream &o, pair<T, U> x) {
+  return o << "(" << x.first << ", " << x.second << ")";
+}
+template <class> struct is_container : false_type {};
+template <class... Ts> struct is_container<vector<Ts...>> : true_type {};
+template <class... Ts> struct is_container<map<Ts...>> : true_type {};
+template <class... Ts> struct is_container<unordered_map<Ts...>> : true_type {};
+template <class... Ts> struct is_container<set<Ts...>> : true_type {};
+template <class... Ts> struct is_container<unordered_set<Ts...>> : true_type {};
+template <class... Ts> struct is_container<multiset<Ts...>> : true_type {};
+template <class... Ts>
+struct is_container<unordered_multiset<Ts...>> : true_type {};
+template <class T, class = typename enable_if<is_container<T>::value>::type>
+ostream &operator<<(ostream &o, T x) {
+  int f = 1;
+  o << "{";
+  for (auto y : x) {
+    o << (f ? "" : ", ") << y;
+    f = 0;
+  }
+  return o << "}";
+}
+
+typedef long long llint;
+
+const int MAXN = 55;
+const int MAXL = 1010;
+
+const map<char, pair<int, int>> DIR = {
+    {'^', {-1, 0}}, {'v', {1, 0}}, {'<', {0, -1}}, {'>', {0, 1}}};
+
+char grid[MAXN][MAXN];
+char moves[MAXL];
+
+int r, s, robot_r, robot_s;
+
+inline void print() {
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < s; ++j)
+      printf("%c", grid[i][j]);
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+}
+
+void move_robot(char dir) {
+  assert(grid[robot_r][robot_s] == '@');
+
+  int rr = robot_r + DIR.at(dir).first;
+  int ss = robot_s + DIR.at(dir).second;
+
+  while (grid[rr][ss] == 'O')
+    rr += DIR.at(dir).first, ss += DIR.at(dir).second;
+
+  if (grid[rr][ss] == '#')
+    return;
+
+  while (true) {
+    swap(grid[rr][ss], grid[rr - DIR.at(dir).first][ss - DIR.at(dir).second]);
+    if (grid[rr][ss] == '@')
+      break;
+    rr -= DIR.at(dir).first;
+    ss -= DIR.at(dir).second;
+  }
+
+  robot_r = rr, robot_s = ss;
+}
+
+void move_robot(const char *moves) {
+  int n = strlen(moves);
+  for (int i = 0; i < n; ++i)
+    move_robot(moves[i]);
+}
+
+int main(void) {
+  while (scanf("%s", grid[r])) {
+    if (r == 0) {
+      s = strlen(grid[r]);
+      ++r;
+      continue;
+    }
+    ++r;
+    bool last = true;
+    for (int i = 0; i < s; ++i)
+      last &= grid[r - 1][i] == '#';
+    if (last)
+      break;
+  }
+
+  for (int i = 0; i < r; ++i)
+    for (int j = 0; j < s; ++j)
+      if (grid[i][j] == '@')
+        robot_r = i, robot_s = j;
+
+  while (scanf("%s", moves) == 1)
+    move_robot(moves);
+
+  print();
+
+  int sol = 0;
+  for (int i = 0; i < r; ++i)
+    for (int j = 0; j < s; ++j)
+      if (grid[i][j] == 'O')
+        sol += 100 * i + j;
+
+  printf("%d\n", sol);
+
+  return 0;
+}
