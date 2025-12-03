54e4647b1b483349285488c1f7e71d9670390374
Matija Bertović <matbertovic@gmail.com>
2025-12-03 10:00:32 +0100

    [AOC2025] Matija Day 2 Part 2 Improved

--- 2025/matija/d02/part2.py
+++ 2025/matija/d02/part2.py
@@ -7,13 +7,7 @@ res = 0
 for s, e in map(lambda x: map(int, x.split("-")), next(sys.stdin).split(",")):
     for i in range(s, e + 1):
         si = str(i)
-        for d in range(1, len(si) // 2 + 1):
-            if len(si) % d == 0:
-                for c in range(1, len(si) // d):
-                    if si[:d] != si[c*d: (c+1)*d]:
-                        break
-                else:
-                    res += i
-                    break
+        if si in (2 * si)[1:-1]:
+            res += i
 
 print(res)
