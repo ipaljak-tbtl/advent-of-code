15dc2180c43aca354279ca8210cad954a9a37de7
Luka <Luka@tbtl.com>
2025-12-05 16:31:43 +0100

    [AOC2025] ltomic day 04 part2 improved

--- 2025/ltomic/day04/part2.py
+++ 2025/ltomic/day04/part2.py
@@ -6,38 +6,41 @@ with open(sys.argv[1], 'r') as f:
 dx = [-1, -1, -1, 0, 1, 1, 1, 0]
 dy = [-1, 0, 1, 1, 1, 0, -1, -1]
 
-sol = 0
-
 def outside(lines, x, y):
     return x < 0 or y < 0 or x >= len(lines) or y >= len(lines[0])
 
 lines = [list(line) for line in lines]
 def adjacent_rolls(lines, x, y):
-    cnt = 0
+    rolls = []
     for px, py in zip(dx, dy):
         nx, ny = x + px, y + py
 
         if outside(lines, nx, ny):
             continue
         if lines[nx][ny] == '@':
-            cnt += 1
+            rolls.append((nx, ny))
 
-    return cnt
+    return rolls
 
-while True:
-    to_remove = []
-
-    for i, line in enumerate(lines):
-        for j, c in enumerate(line):
-            if c == '@' and adjacent_rolls(lines, i, j) < 4:
+cnt = [[0] * len(line) for line in lines]
+to_remove = []
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == '@':
+            cnt[i][j] = len(adjacent_rolls(lines, i, j))
+            if cnt[i][j] < 4:
                 to_remove.append((i, j))
-                sol += 1
 
-    if len(to_remove) == 0:
-        break
+sol = 0
 
-    for i, j, in to_remove:
-        lines[i][j] = '.'
+while len(to_remove) != 0:
+    x, y = to_remove.pop()
+    sol += 1
+
+    for ax, ay in adjacent_rolls(lines, x, y):
+        cnt[ax][ay] -= 1
+        if cnt[ax][ay] == 3:
+            to_remove.append((ax, ay))
 
 print(sol)
 
