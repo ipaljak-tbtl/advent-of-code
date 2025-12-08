93b5e4da71ac1c757a30b34623a5f15cf8e2be61
Ante Derek <ante.derek@gmail.com>
2025-12-08 18:13:09 +0100

    [AOC2025] ante day8

---
+++ 2025/ante/day08/d8b.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define TRACE(x) cerr << #x << " = " << x << endl
+#define _ << " _ " << 
+
+using namespace std;
+
+
+typedef long long llint;
+
+
+int n;
+vector<int> x;
+vector<int> y;
+vector<int> z;
+vector<vector<int>> g;
+const int T = 1000;
+vector<int> bio;
+
+
+int dfs(int v) {
+  bio[v] = 1;
+  int sz = 1;
+  for (int u=0; u<n; u++) {
+    if (g[v][u] && !bio[u]) {
+      sz += dfs(u);
+    }
+  }
+  return sz;
+}
+
+
+int main() {
+  llint a, b, c;
+  while (scanf("%lld,%lld,%lld", &a, &b, &c) == 3) {
+    x.push_back(a);
+    y.push_back(b);
+    z.push_back(c);
+  }
+
+  n = x.size();
+  g = vector<vector<int>>(n, vector<int>(n, 0));
+  
+  vector<pair<llint, pair<int, int>>> dist;
+  for (int i = 0; i < n; i++) 
+    for (int j = i+1; j < n; j++) {
+      llint dx = x[i] - x[j];
+      llint dy = y[i] - y[j];
+      llint dz = z[i] - z[j];
+      llint d = dx*dx + dy*dy + dz*dz;
+      dist.push_back({d, {i, j}});
+    }
+  sort(dist.begin(), dist.end());
+  
+  for (int i=0; ; i++) {
+    g[dist[i].second.first][dist[i].second.second] = 1;
+    g[dist[i].second.second][dist[i].second.first] = 1;
+
+    bio = vector<int>(n, 0);
+    if (dfs(0) == n) {
+      cout << x[dist[i].second.first] * x[dist[i].second.second] << endl;
+      break;
+    }
+  }
+
+  return 0;
+}
